8b41e8aa99e7b8acae53fb6d8a54ca60bde52b1a
simbobs <simonapiademarco@gmail.com>
2022-06-26 18:12:49 +0100

    text edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     return(
         <>
         <h1> Welcome to the Home Page</h1>
-        <p> This is where Simona will eventually write some information about herself and show you some projects that she's worked on for the past 10 weeks. They may not be masterpieces, but they'll show you just how much she's learned in 10 weeks. From a coding noob to a human who has weekly coding nightmares</p>
+        <p> This is where Simona will eventually write some information about herself and show you some projects that she's worked on for the past 10 weeks. They may not be masterpieces, but they'll show you just how much she's learned in 10 weeks. From a coding noob to a human who has weekly coding nightmares.</p>
         </>
     )
 }
